49b6aef94c08c3132456a19ab69e9fb162e8e884
ShuntaToda <todashuntawork@gmail.com>
2023-10-13 15:38:47 +0900

    完成

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\api\LoginController;
+use App\Models\Result;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,41 @@ Route::middleware('auth:sanctum')->middleware("unAuthHandle")->get('/user', func
 
 Route::post("login", [LoginController::class, "login"]);
 
-Route::group(["middleware" => ["unAuthHandle", "auth:samctum"]], function () {
+Route::group(["middleware" => ["unAuthHandle", "auth:sanctum"]], function () {
     Route::get("logout", [LoginController::class, "logout"]);
+    Route::get("fields", function () {
+        return response()->json([
+            "objects" => [
+                [
+                    1, 1, 1, 1, 1
+                ],
+                [
+                    1, 2, 3, 0, 1
+                ],
+                [
+                    1, 3, 0, 4, 1
+                ],
+                [
+                    1, 1, 1, 1, 1
+                ],
+            ]
+        ]);
+    });
+
+    Route::get("results", function () {
+        $results = Result::all();
+        return response()->json($results);
+    });
+    Route::post("results", function (Request $request) {
+        $result = Result::create([
+            "user" => Auth::user()->name,
+            "block_moves" => $request->block_moves,
+            "time" => $request->time,
+        ]);
+
+        return response()->json([
+            "success" => true,
+            $result
+        ]);
+    });
 });
